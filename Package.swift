5684d6743dd617ddeb4b8fa3e11b29c96eb8bf1b
ydh1304 <donghyun.lee@mvlchain.io>
2019-09-24 15:59:27 +0900

    add package.swift

--- Package.swift
+++ Package.swift
@@ -19,8 +19,11 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "h3-ios-framework",
-            dependencies: []),
+            name: "H3",
+            path: ./h3),
+        .target(
+            name: "H3Swift",
+            path: ./H3Swift),
         .testTarget(
             name: "h3-ios-frameworkTests",
             dependencies: ["h3-ios-framework"]),
